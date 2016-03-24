b0d463a0e10fa41713eb1ff2218e4311d1716894
汪鑫源 <xy.wang0317@gmail.com>
2016-03-24 17:44:55 +0800

    Initial Commit

---
+++ PM25/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PM25
+//
+//  Created by 汪鑫源 on 16/3/24.
+//  Copyright © 2016年 汪鑫源. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
